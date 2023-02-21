9810771ce988c495eee9a1651e4e6dfbae86393f
jakiasultana2022 <rokeyasbd@gmail.com>
2023-02-21 11:11:07 +0600

    banner

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Banner.css'
+const Banner = () => {
+    return (
+        <div >
+      <div className="hero ">
+  <div className="hero-overlay bg-opacity-20"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-6xl font-bold font-serif text-red-500 font hover:text-red-700"><span className="mb-5 text-8xl font-semibold font-serif text-red-700  ">H</span>unger Bounce</h1>
+    </div>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import ErrorElement from "../../Pages/ErrorElement/ErrorElement";
 import Home from "../../Pages/Home/Home/Home";
 
 const router= createBrowserRouter([
     {
         path: '/',
+        errorElement: <ErrorElement></ErrorElement>,
         element:<Main></Main>,
   
         children:[
