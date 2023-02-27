a0286afcb6e26b2958dbf2a0600c76bccc79c0d2
jakiasultana2022 <rokeyasbd@gmail.com>
2023-02-27 11:38:05 +0600

    footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,7 +3,37 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            
+           <footer class="bg-gray-900">
+  <div class="container mx-auto py-8 px-4 flex flex-wrap justify-between items-center">
+    <div class="w-full md:w-auto text-center md:text-left mb-4 md:mb-0">
+      <p class="text-red-500 font-bold text-lg">Hunger Bounce</p>
+      <p class="text-gray-500 text-sm">© 2023 All rights reserved.</p>
+    </div>
+    <div class="w-full md:w-auto text-center">
+      <a href="#" class="inline-block mx-4 text-white hover:text-gray-400">
+        About Us
+      </a>
+      <a href="#" class="inline-block mx-4 text-white hover:text-gray-400">
+        Contact Us
+      </a>
+      <a href="#" class="inline-block mx-4 text-white hover:text-gray-400">
+        Privacy Policy
+      </a>
+    </div>
+    <div class="w-full md:w-auto text-center">
+      <a href="#" class="inline-block mx-4 text-red-500 hover:text-gray-400">
+        Facebook
+      </a>
+      <a href="#" class="inline-block mx-4 text-red-500 hover:text-gray-400">
+        Twitter
+      </a>
+      <a href="#" class="inline-block mx-4 text-red-500 hover:text-gray-400">
+        Instagram
+      </a>
+    </div>
+  </div>
+</footer>
+ 
         </div>
     );
 };
--- src/Pages/Shared/Loading/Loading.js
+++ src/Pages/Shared/Loading/Loading.js
@@ -4,7 +4,7 @@ const Loading = () => {
     return (
         <div>
           <div class="flex justify-center items-center h-screen">
-    <div class="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-purple-400 via-blue-500 to-red-400 ">
+    <div class="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-red-500 via-red-800 to-red-500 ">
         <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-gray-200 rounded-full border-2 border-white"></div>
     </div>
 </div>  
